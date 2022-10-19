9b28dc9e3cc29c2fa3693955bb1bb8904bec39a5
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2022-10-19 17:58:52 -0300

    add header and nav

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,10 +4,10 @@ import CV from '../../assets/cv-Dev-FullStack.pdf'
 
 const CTA = () => {
   return (
-    <div>
+    <div className='cta'>
       <a href={CV} download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-      
+
 
     </div>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,25 @@ import './header.css'
 import CTA from './CTA.jsx'
 import ME from '../../assets/me-lol.jpg'
 import HeaderSocials from './HeaderSocials'
+import Fade from 'react-reveal/Fade'
 
 
 const Header = () => {
   return (
     <header>
       <div className="container header-container">
+        <Fade bottom>
         <h5>Hello I'm</h5>
         <h1>Victor Emanuel</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
-        <HeaderSocials />
-
         <div className="me">
           <img src={ME} alt="Programmer-code" />
         </div>
+        </Fade>
+        <HeaderSocials />
+
+        <a href="#contact" className='scrool-down'>Scroll Down</a>
       </div>
     </header>
   )
